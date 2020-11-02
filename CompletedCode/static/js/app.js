db83ca3424391b7f8c6c649eb8c0d2e3f177be4a
QianyueMa <amandama215@gmail.com>
2020-11-01 20:35:17 -0500

    updated

--- CompletedCode/static/js/app.js
+++ CompletedCode/static/js/app.js
@@ -1,71 +1,140 @@
-// Use the D3 library to read data in data.json.
+// Use the D3 library to read in data samples.json.
 
+// "names"
+// Read "names" from json and display them in a dropdown menu
+function getName() {
+
+    // Select dropdown menu id and assign it to a variable
+    var dropdownMenu = d3.select('#selDataset');
+
+    // Read "names" values and append them into dropdown menu
+    // Update all of the plots any time that a new sample is selected.
+    d3.json('data/samples.json')
+        .then(subject => subject.names
+            .forEach(name => dropdownMenu
+                .append('option')
+                .text(name)
+                .property('value'),
+
+                // Initialize page with default metadata and plots   
+                getMetadata(subject.names[0]),
+                buildCharts(subject.names[0]),
+            ),
+        );
+};
+getName();
+
+
+// "metadata"
 // Display the sample metadata, i.e., an individual's demographic information.
-// {"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0}, ...
-function getMetadata() {
-    d3.json("data/samples.json").then( (data) => {
-        var demographicInfo = d3.select("#sample-metadata").html("");
-        // Display each key-value pair from the metadata JSON object somewhere on the page.
-        Object.entries(data).forEach(([key, value]) => {   
-            demographicInfo.append("h5").text(`${key}:${value}`);
-        });
+// e.g., {"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0}, ...
+function getMetadata(id) {
+    
+    // Read "metadata" from json file for each subject and assign it to a variable
+    d3.json('data/samples.json').then( (data) => {
+
+        var subjectData = data.metadata
+            .filter(subject => subject.id.toString() === id)[0];
+
+        // Select demographic info id and assign it to a variable
+        var subjectDemographic = d3.select('#sample-metadata');
+
+        // Clear the metadata before displaying a new set of selected data 
+        subjectDemographic.html('');
+
+        // Push each key-value pair from the metadata JSON object onto into demographic info card.
+        Object.entries(subjectData)
+            .forEach(([key, value]) => subjectDemographic
+                .append('h5')
+                .text(`${key}: ${value}`),
+            );
     });
 };
+getMetadata();
 
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-function buildChart() {
 
-    var url = `data/data.json/${samples}`;
+// Function called by DOM changes
+function optionChanged(id) {
+    getMetadata(id);
+    buildCharts(id);
+};
 
-    d3.json(url).then( (data) => {
+
+getName();
+
+
+// Create a horizontal bar chart and a bubble chart to display the top 10 OTUs found in that individual. 
+function buildCharts(id) {
+
+    // Read data from json for plotting
+    d3.json('data/samples.json').then( (data) => {
+
+        // Dataset structure: names, metadata, samples
+        // -> "samples": id, otu_ids, sample_values, otu_labels
+        var nameResults = data.samples
+            .filter(sample => sample.id === id)[0];
+
+        // Check out the console for a friendlier layout
+        console.log("Participant X's data:")
+        console.log(nameResults);
+
+        // Extract otu_ids, sample_values, otu_labels data
+        var sampleValues = nameResults.sample_values; // Use sample_values as the values for the bar chart.
+        var outIds = nameResults.otu_ids; // Use otu_ids as the labels for the bar chart.
+        var outLabels = nameResults.otu_labels // Use otu_labels as the hovertext for the chart.
         
-        var otuId = data.otu_ids;
-        // Sort and slice the top 10 OTUs in descending order, and reverse the array to accommodate Plotly's defaults
-        var results = data.sort( (a, b) => b.otuId - a.otuId).slice(0, 10).reverse();
-
-        var otuTop = otuId.map(d => d);
-        var sampleValue = results.sample_values;
-        var otuLabel = results.otu_labels;
-
-        var trace1 = {
-            // Use otu_ids as the labels for the bar chart.
-            x: otuTop,
-            // Use sample_values as the values for the bar chart.
-            y: sampleValue,
-            // Use otu_labels as the hovertext for the chart.
-            type: "bar",
-            orientation: "h",
-            test: otuLabel
+        // Trace for bar chart that displays each sample values
+        var barTrace = {
+            // Sort and slice the top 10 OTUs in descending order, and reverse the array to accommodate Plotly's defaults
+            x: sampleValues.slice(0, 10).reverse(), // x values
+            y: outIds.slice(0, 10).map(otuid => `OTU ${otuid}`).reverse(), // y values
+            text: outLabels.slice(0, 10).reverse(), // the hover texts
+            hoverlabel: { font: { size: 12 } },
+            marker: {
+                color: 'blue',
+                opacity: 0.8,
+            }, // the colour of the bars
+            type: 'bar',
+            orientation: 'h' // a horizontal bar chart
         };
 
-        // Create a bubble chart that displays each sample.
-        var trace2 = {
-            x: otuTop,
-            y: sampleValue,
-            mode:"markers", 
-            marker:{
-              size: sampleValue,
-              color: otuTop,
-              colorscale: "Rainbow",
-              labels: otuLabel,
-              type: 'scatter',
-              opacity: 0.3
-            }
+        // Trace for bubble chart
+        var bubbleTrace = {
+            x: outIds,
+            y: sampleValues,
+            text: outLabels,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: outIds,
+                colorscale: "Rainbow",
+                opacity: 0.5,
+            },
         };
 
-        var data1 = [trace1];
-        var data2 = [trace2];
+        // Data
+        var barData = [barTrace];
+        var bubbleData = [bubbleTrace];
 
-        var layout = {
-            title: "Top 10 OTUs Found in the Participant",
+        // Layout set up
+        var Layout = {
+            title: {
+                text: `Top 10 OTUs Found in the Subject ${id}`,
+                font: {
+                    family: 'Arial',
+                    size: 24,
+                    color: 'navy'
+                    },
+                },
             xaxis: { title: 'OTU ID' },
-        };
-
-        // Plot the chart
-        Plotly.newPlot("bar", data1, layout);
+            yaxis: { title: 'Sample Values' }, 
+            };
         
-        Plotly.newPlot("bubble", data2, layout);
+        // Plot the chart
+        Plotly.newPlot('bar', barData, Layout);
+        Plotly.newPlot('bubble', bubbleData, Layout);
 
     });
 };
 
+buildCharts();
